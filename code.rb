0777566167f6684e1460ea0c7b23c5fdd871a756
sallypikachu <sallypikachu@gmail.com>
2015-11-21 23:40:23 -0500

    Initial commit

---
+++ code.rb
@@ -0,0 +1,28 @@
+require 'csv'
+require 'pry'
+
+#createdb ingredients
+
+require "pg"
+
+def db_connection
+  begin
+    connection = PG.connect(dbname: "ingredients")
+    yield(connection)
+  ensure
+    connection.close
+  end
+end
+
+# db_connection do |conn|
+#   conn.exec("CREATE TABLE ingredient(
+#   id SERIAL PRIMARY KEY,
+#   food varchar(100));")
+# end
+
+array_of_array = []
+CSV.foreach('ingredients.csv') do |row|
+  binding.pry
+  ingredient = row.to_a
+  array_of_array << ingredient
+end
